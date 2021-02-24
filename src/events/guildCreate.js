aecf136f4e5fb44d00a7a07df0321524fd05d560
Ioannis Tziligkakis <gtsiligkakis@gmail.com>
2021-02-25 01:44:59 +0200

    Remove events JavaScript modules

--- src/events/guildCreate.js
+++
@@ -1,9 +0,0 @@
-const config = require("../config");
-const { UpdateGuildSettings } = require("../lib/resourceRepo");
-
-module.exports = () => async (guild) => {
-  await UpdateGuildSettings(
-    guild.id,
-    JSON.stringify(config.defaultClientSettings)
-  );
-};
