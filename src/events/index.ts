0dee740da887f2df3a2b82398c3d2ac4aa3afa07
Ioannis Tziligkakis <gtsiligkakis@gmail.com>
2021-02-25 01:44:06 +0200

    Migrate events exports to TypeScript

---
+++ src/events/index.ts
@@ -0,0 +1,6 @@
+export { default as GuildBanAdd } from "./GuildBanAdd";
+export { default as GuildBanRemove } from "./GuildBanRemove";
+export { default as GuildCreate } from "./GuildCreate";
+export { default as GuildDelete } from "./GuildDelete";
+export { default as GuildMemberAdd } from "./GuildMemberAdd";
+export { default as MessageCreate } from "./MessageCreate";
